cf46a6feea458bdbbe94ac90019ad39d8d6ea9ab
oksanamatiushenko <ksuzzxx3@gmail.com>
2025-11-24 11:12:04 +0100

    init project

---
+++ src/pages/Navigation.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "./HomePage/HomePage";
+import LoginPage from "./LoginPage/LoginPage";
+import RegisterPage from "./RegisterPage/RegisterPage";
+import NotFoundPage from "./NotFoundPage/NotFoundPage";
+
+const Navigation = () => {
+  return (
+    <>
+      <Navigation>
+        <Routes>
+            <Route path="/" element={<HomePage/>} />
+            <Route path="/login" element={<LoginPage/>} />
+            <Route path="/register" element={<RegisterPage/>} />
+            <Route path="/notfound" element={<NotFoundPage/>} />
+        </Routes>
+      </Navigation>
+    </>
+  );
+};
+
+export default Navigation;
